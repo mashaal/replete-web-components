dd3ced7fed6643ff60eb47666b92c428ac17b8ee
Omar Mashaal <omar@teacups.io>
2024-03-19 23:13:55 +1100

    update component

--- components/button.element.js
+++ components/button.element.js
@@ -1,39 +1,30 @@
-const template = `<link rel="stylesheet" href="${import.meta.resolve(
-  './button.element.css'
-)}"/><button><slot></slot></button>`;
+const html = String.raw;
+
+const css = import.meta.resolve('./button.element.css');
+
+const template = html`<link rel="stylesheet" href="${css}" />
+  <button>
+    <slot></slot>
+  </button>`;
 
 export { template };
 
-if (typeof document !== 'undefined') {
-  class ExButton extends HTMLElement {
-    constructor() {
-      super();
-
-      const internals = this.attachInternals();
-
-      // check for a Declarative Shadow Root:
-      let shadow = internals.shadowRoot;
-
-      if (!shadow) {
-        // there wasn't one. create a new Shadow Root:
-        shadow = this.attachShadow({
-          mode: 'open',
-        });
-
-        shadow.innerHTML = template;
-      }
-
-      console.log(shadow);
-    }
-    connectedCallback() {
-      const button = this?.shadowRoot?.querySelector('button');
-      button?.addEventListener('click', () => {
-        alert('🐦‍🔥');
-      });
+class ExButton extends HTMLElement {
+  constructor() {
+    super();
+    let shadow = this.attachInternals().shadowRoot;
+    if (!shadow) {
+      shadow = this.attachShadow({ mode: 'open' });
+      shadow.innerHTML = template;
     }
   }
-
-  if (!customElements.get('ex-button')) {
-    customElements.define('ex-button', ExButton);
+  connectedCallback() {
+    const button = this?.shadowRoot?.querySelector('button');
+    button?.addEventListener('click', () => {
+      alert('🐦‍🔥');
+    });
   }
+}
+if (!customElements.get('ex-button')) {
+  customElements.define('ex-button', ExButton);
 }
