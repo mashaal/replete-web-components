c361c32e5520ace0437b5feae6da0a1aadfc606f
Omar Mashaal <omar@teacups.io>
2024-03-20 09:37:10 +1100

    updates to static

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,4 +1,5 @@
 import { template } from './button.element.js';
+import { raw } from '@kyiro/hono/html';
 
 /**
  * @type {import("hono/jsx").FC}
@@ -7,7 +8,7 @@ const Button = (props) => {
   return (
     <>
       <ex-button>
-        <template shadowrootmode="open">{template}</template>
+        <template shadowrootmode="open">{raw(template)}</template>
         {props.children}
       </ex-button>
       <script type="module" src="/components/button.element.js"></script>
@@ -16,7 +17,6 @@ const Button = (props) => {
 };
 
 export default Button;
-
 //demo import React from 'https://esm.sh/react';
 //demo import { renderToStaticMarkup } from 'https://esm.sh/react-dom/server';
 //demo const string = renderToStaticMarkup(<Button>Click me pls</Button>);
--- components/button.element.js
+++ components/button.element.js
@@ -1,6 +1,6 @@
 const html = String.raw;
 
-const css = import.meta.resolve('./button.element.css');
+const css = './components/button.element.css';
 
 const template = html`<link rel="stylesheet" href="${css}" />
   <button>
@@ -9,22 +9,24 @@ const template = html`<link rel="stylesheet" href="${css}" />
 
 export { template };
 
-class ExButton extends HTMLElement {
-  constructor() {
-    super();
-    let shadow = this.attachInternals().shadowRoot;
-    if (!shadow) {
-      shadow = this.attachShadow({ mode: 'open' });
-      shadow.innerHTML = template;
+if (typeof document !== 'undefined') {
+  class ExButton extends HTMLElement {
+    constructor() {
+      super();
+      let shadow = this.attachInternals().shadowRoot;
+      if (!shadow) {
+        shadow = this.attachShadow({ mode: 'open' });
+        shadow.innerHTML = template;
+      }
+    }
+    connectedCallback() {
+      const button = this?.shadowRoot?.querySelector('button');
+      button?.addEventListener('click', () => {
+        confirm('🐦‍🔥');
+      });
     }
   }
-  connectedCallback() {
-    const button = this?.shadowRoot?.querySelector('button');
-    button?.addEventListener('click', () => {
-      alert('🐦‍🔥');
-    });
+  if (!customElements.get('ex-button')) {
+    customElements.define('ex-button', ExButton);
   }
-}
-if (!customElements.get('ex-button')) {
-  customElements.define('ex-button', ExButton);
 }
